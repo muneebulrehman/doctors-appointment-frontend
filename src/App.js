cd0137cbe00a76b695e5fcb8c93d7eb3f7d75944
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-14 15:26:56 +0530

    Finished fetching doctors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
+import DoctorView from './features/doctor/DoctorView';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <h1>React app</h1>
+        <DoctorView />
         <Routes />
       </div>
     </BrowserRouter>
--- src/features/doctor/doctorSlice.js
+++ src/features/doctor/doctorSlice.js
@@ -1,4 +1,4 @@
-import { creatSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
   doctors: [],
@@ -21,32 +21,32 @@ export const fetchSingleDoctor = createAsyncThunk('user/fetchSingleDoctor', asyn
   return doctor;
 });
 
-const doctorSlice = creatSlice({
+const doctorSlice = createSlice({
   name: 'doctor',
   initialState,
   extraReducers: (builder) => {
-    builder.addcase(fetchUsers.pending, (state) => {
+    builder.addCase(fetchDoctors.pending, (state) => {
       state.loading = true;
     });
-    builder.addcase(fetchUsers.fulfilled, (state, action) => {
-      state.doctors = [...action.payload];
+    builder.addCase(fetchDoctors.fulfilled, (state, action) => {
+      state.doctors = action.payload;
       state.loading = false;
       state.error = '';
     });
-    builder.addcase(fetchUsers.rejected, (state, action) => {
+    builder.addCase(fetchDoctors.rejected, (state, action) => {
       state.error = action.error.message;
       state.loading = false;
       state.doctors = [];
     });
-    builder.addcase(fetchSingleDoctor.pending, (state) => {
+    builder.addCase(fetchSingleDoctor.pending, (state) => {
       state.loading = true;
     });
-    builder.addcase(fetchSingleDoctor.fulfilled, (state, action) => {
+    builder.addCase(fetchSingleDoctor.fulfilled, (state, action) => {
       state.doctor = action.payload;
       state.loading = false;
       state.error = '';
     });
-    builder.addcase(fetchSingleDoctor.rejected, (state, action) => {
+    builder.addCase(fetchSingleDoctor.rejected, (state, action) => {
       state.error = action.error.message;
       state.loading = false;
       state.doctor = {};
