d6172175e108b2500a1b01f2513ab2b2231418fd
backtofayton <yucelolcay@gmail.com>
2022-07-14 23:10:28 +0300

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Routes } from 'react-router-dom';
-import './App.css';
-import DoctorView from './features/doctor/DoctorView';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import DoctorView from "./features/doctor/DoctorView";
+import AppointmentShow from "./features/appointment/AppointmentShow";
+import AppointmentsIndex from "./features/appointment/AppointmentsIndex";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <h1>React app</h1>
-        <DoctorView />
-        <Routes />
+        <Routes>
+          <Route path="/" element={<DoctorView />}>
+            <Route path="appointments" element={<AppointmentsIndex />}>
+              <Route path=":appointmentId" element={<AppointmentShow />} />
+            </Route>
+          </Route>
+        </Routes>
       </div>
     </BrowserRouter>
   );
