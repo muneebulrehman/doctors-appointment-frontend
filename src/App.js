91dc5f51a52ecaeb31639c146df91d0f4190906d
backtofayton <yucelolcay@gmail.com>
2022-07-15 00:57:50 +0300

    Navbar.jsx

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import DoctorView from './features/doctor/DoctorView';
 import AppointmentShow from './features/appointment/AppointmentShow';
 import AppointmentsIndex from './features/appointment/AppointmentsIndex';
+import NavBar from './features/layout/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="App d-flex">
         <h1>React app</h1>
+        <NavBar />
         <Routes>
           <Route path="/" element={<DoctorView />} />
           <Route path="/appointments" element={<AppointmentsIndex />} />
