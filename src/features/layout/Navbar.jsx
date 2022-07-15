b030c14407bc321d4481c1c85139468ee408e830
backtofayton <yucelolcay@gmail.com>
2022-07-15 14:32:18 +0300

    NavBar

--- src/features/layout/Navbar.jsx
+++ src/features/layout/Navbar.jsx
@@ -1,14 +1,12 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import { toggleNavMenu } from './layoutSlice';
 import styles from './Navbar.module.scss';
 
 export default function NavBar() {
   const { navMenuIsOpen } = useSelector((state) => state.layout);
   const dispatch = useDispatch();
-
-  function menuClickHandler() {
-    dispatch(toggleNavMenu());
-  }
 
   return (
     <>
@@ -24,19 +22,38 @@ export default function NavBar() {
         className="position-absolute d-none"
         type="button"
         id="menu-burger-button"
-        onClick={menuClickHandler}
+        onClick={() => dispatch(toggleNavMenu())}
         aria-label="menu-burger"
       />
       <nav
-        className={`${
-          styles.navBar
-        }} bg-white border-end border-light border-2 position-absolute top-0\
-       bottom-0 ${navMenuIsOpen ? '' : styles.hideLeft}`}
+        className={`${styles.navBar} ${navMenuIsOpen ? '' : styles.hideLeft}`}
       >
-        <ul id="nav-list" className={`ps-3 text-start ${styles.navList}`}>
-          <li>DOCTORS</li>
-          <li>APPOINTMENTS</li>
-        </ul>
+        <div className="bg-white border-end border-light border-1 d-flex flex-column pt-4">
+          <FontAwesomeIcon className="h-2rem" icon="fa-briefcase-medical" />
+          <ul
+            id="nav-list"
+            className={`ps-3 text-start flex-grow-1 ${styles.navList}`}
+          >
+            <li>DOCTORS</li>
+            <li>APPOINTMENTS</li>
+          </ul>
+          <ul className="d-flex gap-2 justify-content-center pb-2 mb-1 px-0">
+            <li>
+              <FontAwesomeIcon icon="fa-brands fa-twitter" />
+            </li>
+            <li>
+              <FontAwesomeIcon icon="fa-brands fa-facebook-f" />
+            </li>
+          </ul>
+          <sub className="mb-5">&copy; 2022 Remote Developers</sub>
+        </div>
+        <button
+          type="button"
+          className={styles.closeNavMenu}
+          onClick={() => dispatch(toggleNavMenu())}
+        >
+          <FontAwesomeIcon icon="fa-caret-left" />
+        </button>
       </nav>
     </>
   );
