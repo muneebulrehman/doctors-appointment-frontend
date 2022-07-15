0a1190fcdd2df91015916f8163098635a94339ec
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-15 06:16:11 +0530

    Added reducers

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,10 +1,13 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
+import { getCookie } from '../../util/cookies';
+
 const url = 'https://doctors-appointment-backend.herokuapp.com/api/';
 
 const initialState = {
   user: null,
-  loading: false
+  loading: false,
+  error: ''
 };
 
 export const createUser = createAsyncThunk('user/createUser', async (user) => {
@@ -29,4 +32,35 @@ export const login = createAsyncThunk('user/login', async (user) => {
   });
   const data = await response.json();
   return data;
+});
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(createUser.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(createUser.fulfilled, (state, action) => {
+      state.user = action.payload;
+      state.loading = false;
+    });
+    builder.addCase(createUser.rejected, (state, action) => {
+      state.error = action.error.message;
+      state.loading = false;
+      state.user = null;
+    });
+    builder.addCase(login.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.user = action.payload;
+      state.loading = false;
+      getCookie();
+    });
+    builder.addCase(login.rejected, (state, action) => {
+      state.error = action.error.message;
+      state.loading = false;
+    });
+  }
 });
