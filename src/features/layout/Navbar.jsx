52ff4e2d32cfa480f545baeae10c3af6d3b31dfa
backtofayton <yucelolcay@gmail.com>
2022-07-15 12:42:20 +0300

    CSS

--- src/features/layout/Navbar.jsx
+++ src/features/layout/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleNavMenu } from './layoutSlice';
+import styles from './Navbar.module.scss';
 
 export default function NavBar() {
   const { navMenuIsOpen } = useSelector((state) => state.layout);
@@ -10,11 +11,33 @@ export default function NavBar() {
   }
 
   return (
-    <div className="bg-white border-end border-light border-2 position-absolute top-0 bottom-0">
-      <ul id="nav-list" className="ps-3 text-start">
-        <li>DOCTORS</li>
-        <li>APPOINTMENTS</li>
-      </ul>
-    </div>
+    <>
+      <label
+        id="menu-burger"
+        htmlFor="menu-burger-button"
+        className={`position-absolute ${styles.menuBurger}`}
+      >
+        <div />
+        <div />
+      </label>
+      <button
+        className="position-absolute d-none"
+        type="button"
+        id="menu-burger-button"
+        onClick={menuClickHandler}
+        aria-label="menu-burger"
+      />
+      <nav
+        className={`${
+          styles.navBar
+        }} bg-white border-end border-light border-2 position-absolute top-0\
+       bottom-0 ${navMenuIsOpen ? '' : styles.hideLeft}`}
+      >
+        <ul id="nav-list" className={`ps-3 text-start ${styles.navList}`}>
+          <li>DOCTORS</li>
+          <li>APPOINTMENTS</li>
+        </ul>
+      </nav>
+    </>
   );
 }
