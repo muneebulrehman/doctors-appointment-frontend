eb8a573bdcf0b1a45f3f6e5778bc8961ee0ae742
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-18 17:26:10 +0530

    Fixing merge issues

--- src/features/doctor/doctorSlice.js
+++ src/features/doctor/doctorSlice.js
@@ -3,27 +3,22 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 const initialState = {
   doctors: [],
   doctor: {},
-  loading: false,
+  loading: false
 };
 
 export const fetchDoctors = createAsyncThunk('user/fetchDoctors', async () => {
-  const response = await fetch(
-    'https://doctors-appointment-backend.herokuapp.com/api/doctors'
-  );
+  const response = await fetch('https://doctors-appointment-backend.herokuapp.com/api/doctors');
   const doctors = await response.json();
   return doctors;
 });
 
-export const fetchSingleDoctor = createAsyncThunk(
-  'user/fetchSingleDoctor',
-  async (id) => {
-    const response = await fetch(
-      `https://doctors-appointment-backend.herokuapp.com/api/doctors/${id}`
-    );
-    const doctor = await response.json();
-    return doctor;
-  }
-);
+export const fetchSingleDoctor = createAsyncThunk('user/fetchSingleDoctor', async (id) => {
+  const response = await fetch(
+    `https://doctors-appointment-backend.herokuapp.com/api/doctors/${id}`
+  );
+  const doctor = await response.json();
+  return doctor;
+});
 
 const doctorSlice = createSlice({
   name: 'doctor',
@@ -45,12 +40,7 @@ const doctorSlice = createSlice({
       state.loading = false;
       state.error = '';
     });
-    builder.addCase(fetchSingleDoctor.rejected, (state, action) => {
-      state.error = action.error.message;
-      state.loading = false;
-      state.doctor = {};
-    });
-  },
+  }
 });
 
 export default doctorSlice.reducer;
