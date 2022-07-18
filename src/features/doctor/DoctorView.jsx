bed498eb4cf6e33a2066d46b29ebb38997b1c369
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-18 21:33:19 +0530

    Finished doctor view

--- src/features/doctor/DoctorView.jsx
+++ src/features/doctor/DoctorView.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 import { fetchSingleDoctor } from './doctorSlice';
+import './doctors.css';
 
 const DoctorView = () => {
   const doctor = useSelector((state) => state.doctor.doctor);
@@ -13,7 +14,23 @@ const DoctorView = () => {
   }, []);
   return (
     <div className="doctorView-container">
-      <h2>{doctor.name}</h2>
+      <div className="doctorView-image-container">
+        <img
+          src={doctor.photo}
+          alt={doctor.name}
+          className="doctorView-image"
+        />
+      </div>
+      <div className="doctorView-info">
+        <h4>{doctor.name}</h4>
+        <p>{doctor.speciality}</p>
+        <p>Price: ${doctor.price}</p>
+        <h6 style={{ textAlign: 'left', fontWeight: 'bold' }}>Bio</h6>
+        <p style={{ textAlign: 'left' }}>{doctor.bio}</p>
+        <Link to="/new_appointment" className="doctorView-reserve">
+          Book appointment
+        </Link>
+      </div>
     </div>
   );
 };
