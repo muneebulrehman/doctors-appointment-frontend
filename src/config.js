6c985ad1605f6d11f22c5847066cecad688fb0d0
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-21 13:54:50 +0530

    Fixed error

--- src/config.js
+++ src/config.js
@@ -4,11 +4,4 @@ export default {
       ? 'https://doctors-appointment-backend.herokuapp.com/api'
       : '/api',
   mobileBreakPoint: 767.98,
-  baseUrl:
-    process.env.NODE_ENV === 'production'
-      ? 'https://doctors-appointment-backend.herokuapp.com/api'
-      : '/api',
-  // baseUrl: 'http://localhost:3001/api',
 };
-
-// export const baseUrl =
