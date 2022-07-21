800c5d90fbc4f2aab44974d31c4f7fd8a4dbaccb
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-21 17:42:34 +0530

    Removed cors proxy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   url:
     process.env.NODE_ENV === 'production'
-      ? 'https://evening-ravine-92387.herokuapp.com/https://doctors-appointment-backend.herokuapp.com/api'
+      ? 'https://doctors-appointment-backend.herokuapp.com/api'
       : '/api',
   mobileBreakPoint: 767.98,
 };
