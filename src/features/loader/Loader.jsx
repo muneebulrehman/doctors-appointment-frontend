e90fa56215dcee8e47326498682c4ed36d536056
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-21 20:07:35 +0530

    Fixed netlify deploy error

--- src/features/loader/Loader.jsx
+++ src/features/loader/Loader.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import './loader.css';
 
-const Loader = () => {
-  return (
-    <div className="loader-container">
-      <div className="spinner">
-        <span>L</span>
-        <span>O</span>
-        <span>A</span>
-        <span>D</span>
-        <span>I</span>
-        <span>N</span>
-        <span>G</span>
-      </div>
+const Loader = () => (
+  <div className="loader-container">
+    <div className="spinner">
+      <span>L</span>
+      <span>O</span>
+      <span>A</span>
+      <span>D</span>
+      <span>I</span>
+      <span>N</span>
+      <span>G</span>
     </div>
-  );
-};
-
+  </div>
+);
 export default Loader;
